f7b965aa6297fbdb7e5106659f35cf2ef8ec97f1
bmoreira <bmoreira@student.42sp.org.br>
2026-04-06 20:05:31 -0300

    started ex01: phonebook

---
+++ ex01/src/main.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main(void)
+{
+    std::cout << "---------PHONEBOOK---------" << std::endl;
+    std::cout << "- Choose an option: -------" << std::endl;
+    std::cout << "- 1. Add a contact --------" << std::endl;
+    std::cout << "- 2. Search a contact -----" << std::endl;
+    std::cout << "- 3. Exit -----------------" << std::endl;
+    std::cout << "---------------------------" << std::endl;
+}
