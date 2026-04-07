ece52de0fdeb1d7526003135a6af6836c9b2e4a1
bmoreira <bmoreira@student.42sp.org.br>
2026-04-06 23:19:31 -0300

    feat: implemented searchContact in phonebook. missing index incrementing

--- ex01/include/PhoneBook.hpp
+++ ex01/include/PhoneBook.hpp
@@ -1,12 +1,16 @@
 #ifndef PHONEBOOK_HPP
 #define PHONEBOOK_HPP
 
+#include <iostream>
+#include <sstream>
 #include "Contact.hpp"
 
 class PhoneBook {
 	private:
-		Contact contacts[8];
-		int		index;
+		int			index;
+		Contact 	contacts[8];
+		std::string formatResult(std::string text);
+		std::string toString(int num);
 	public:
 		PhoneBook();
 		void addContact();
--- ex01/src/PhoneBook.cpp
+++ ex01/src/PhoneBook.cpp
@@ -1,4 +1,3 @@
-#include <iostream>
 #include "PhoneBook.hpp"
 
 PhoneBook::PhoneBook() : index(0) {}
@@ -10,6 +9,7 @@ void PhoneBook::addContact(){
     std::string phoneNumber = "";
     std::string darkestSecret = "";
 
+	std::cin.ignore();
 	while (firstName.empty())
 	{
 		std::cout << "- First name: ";
@@ -42,14 +42,52 @@ void PhoneBook::addContact(){
 							phoneNumber,
 							darkestSecret
 						);
+	PhoneBook::index++; // verificar por que nao está incrementando
 	std::cout << "---------------------------" << std::endl;
 	std::cout << "----- Contact added! ------" << std::endl;
+	std::cout << std::endl;
 }
 
 void PhoneBook::searchContact(){
 
+	std::cout << "_________________ PHONEBOOK _________________" << std::endl;
+	std::cout << "|          |          |          |          |" << std::endl;
+	std::cout << "|INDEX     |NAME      |LAST NAME |NICKNAME  |" << std::endl;	
+	std::cout << index << std::endl;
+	for (int i = 0; i < index; i++)
+	{
+		std::cout << "|" << formatResult(toString(i)) << "|";
+		std::cout << formatResult(contacts[i].getFirstName()) << "|";
+		std::cout << formatResult(contacts[i].getLastName()) << "|";
+		std::cout << formatResult(contacts[i].getNickname()) << "|";
+	}
+	std::cout << "|__________|__________|__________|__________|" << std::endl;
 }
 
 void PhoneBook::displayContact(int index){
+	std::cout << index;
+}
 
+std::string PhoneBook::formatResult(std::string text)
+{
+	std::string result;
+	int length;
+	int	max_c;
+
+	max_c = 10;
+	length = text.length();
+	result = text;
+	if (length <= max_c)
+		for (int i = 0; i < max_c - length; i++)
+			result += " ";
+	else
+		 result = text.substr(0, 8) + ".";
+	return (result);
+}
+
+std::string PhoneBook::toString(int num)
+{
+	std::stringstream ss;
+	ss << num;
+	return (ss.str());
 }
