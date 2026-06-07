d0b14c4418a09cef35640d0a52c43101b9bdeed0
bmoreira <bmoreira@student.42sp.org.br>
2026-06-07 02:47:52 -0300

    starting zombieHorde

--- cpp01/ex01/src/zombieHorde.cpp
+++ cpp01/ex01/src/zombieHorde.cpp
@@ -1,6 +1,6 @@
 #include "Zombie.hpp"
 
 Zombie* zombieHorde( int N, std::string name ){
-	Zombie *zombie = new Zombie(name)[N];
-	
+	Zombie *zombie = new Zombie[N];
+	// percorrer zombies com for e setar name
 }
