eed54c73f259e113dd9516cd31c07d245c862a49
bmoreira <bmoreira@student.42sp.org.br>
2026-06-07 02:43:57 -0300

    feat: started cpp01/ex01

---
+++ cpp01/ex01/src/Zombie.cpp
@@ -0,0 +1,14 @@
+#include "Zombie.hpp"
+
+Zombie::Zombie() {}
+
+Zombie::Zombie( std::string name ) : name(name) {}
+
+Zombie::~Zombie()
+{
+	std::cout << name << std::endl;
+}
+
+void Zombie::announce(){
+	std::cout << name << ": BraiiiiiiinnnzzzZ..." << std::endl;
+}
