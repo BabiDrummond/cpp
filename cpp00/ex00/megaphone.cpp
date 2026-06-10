55e4f8ba170d6b0bb57963d821b17bedd20096ad
bmoreira <bmoreira@student.42sp.org.br>
2026-06-10 20:57:48 -0300

    fix: issue in megaphone when multiple strings

--- cpp00/ex00/megaphone.cpp
+++ cpp00/ex00/megaphone.cpp
@@ -2,25 +2,20 @@
 #include <string>
 #include <cctype>
 
-void    megaphone(int argc, char **argv)
+void    megaphone(std::string word)
 {
-    std::string word;
-
-    for (int i = 0; i < argc; i++)
-    {
-        word = argv[i];
-        for (size_t j = 0; j < word.length(); j++)
-            word[j] = std::toupper(word[j]);
-        std::cout << word;
-    }
-    std::cout << std::endl;
+    for (size_t j = 0; j < word.length(); j++)
+        word[j] = std::toupper(word[j]);
+    std::cout << word << " ";
 }
 
 int main(int argc, char **argv)
 {
     if (argc < 2)
-        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *" << std::endl;
+        std::cout << "* LOUD AND UNBEARABLE FEEDBACK NOISE *";
     else
-        megaphone(argc - 1, argv + 1);
+        for (int i = 1; i < argc; i++)
+            megaphone(argv[i]);
+    std::cout << std::endl;
     return (0);
 }
