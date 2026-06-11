058058b32f40f70b9d80d086187a62cb35b7f567
bmoreira <bmoreira@student.42sp.org.br>
2026-06-10 22:01:31 -0300

    finished ex02

--- cpp01/ex02/src/main.cpp
+++ cpp01/ex02/src/main.cpp
@@ -4,6 +4,14 @@
 int	main(void)
 {
 	std::string string = "HI THIS IS BRAIN";
-	std::string stringPTR;
-	std::string stringREF;
+	std::string *stringPTR = &string;
+	std::string& stringREF = string;
+
+	std::cout << &string << std::endl;
+	std::cout << stringPTR << std::endl;
+	std::cout << &stringREF << std::endl;
+
+	std::cout << string << std::endl;
+	std::cout << *stringPTR << std::endl;
+	std::cout << stringREF << std::endl;
 }
