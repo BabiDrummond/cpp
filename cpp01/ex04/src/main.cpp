8b2ca239246b3574f27c858236b1caff7fb5c1b3
bmoreira <bmoreira@student.42sp.org.br>
2026-07-07 22:08:47 -0300

    finished ex04

--- cpp01/ex04/src/main.cpp
+++ cpp01/ex04/src/main.cpp
@@ -3,26 +3,36 @@
 #include <sstream>
 #include <fstream>
 
-void readFile(const char *fileName, std::string s1, std::string s2)
+bool dumpFile(const std::string& fileName, std::string& content)
 {
-	std::ifstream filein(fileName);
+	std::ifstream filein(fileName.c_str());
 	if (!filein.is_open())
-	{
-		std::cout << "Error to open file." << std::endl;
-		return ;
-	}
-	
+		return (false);
+
 	std::stringstream buffer;
 	buffer << filein.rdbuf();
-	std::string content = buffer.str();
-
-	std::size_t pos = content.find(s1);
-	if (pos != std::string::npos)
-		std::cout << s1 << " found in pos: " << pos << std::endl;
-	else
-		std::cout << "Could not find " << s1 << std::endl;
-	(void) s2;
+	content = buffer.str();
 	filein.close();
+
+	return (true);
+}
+
+void replaceStr(std::string fileName, std::string content, std::string s1, std::string s2)
+{
+	const std::string outputFile = fileName.append(".replace");
+	std::ofstream fileout(outputFile.c_str());
+
+	std::size_t start = 0;
+	std::size_t end;
+
+	while ((end = content.find(s1, start)) != std::string::npos) {
+		fileout.write(content.c_str() + start, end - start);
+		fileout << s2;
+		start = end + s1.length();
+	}
+	end = content.length();
+	fileout.write(content.c_str() + start, end - start);
+	fileout.close();
 }
 
 int main(int argc, char **argv)
@@ -31,5 +41,13 @@ int main(int argc, char **argv)
 		std::cout << "Please provide all parameters: ./sed [path/fileName] [s1] [s2]" << std::endl;
 		return (1);
 	}
-	readFile(argv[1], (std::string)argv[2], (std::string)argv[3]);
+
+	std::string fileContent;
+	if (!dumpFile(argv[1], fileContent)){
+		std::cout << "Error to open file." << std::endl;
+		return (1);
+	}
+	replaceStr(argv[1], fileContent, argv[2], argv[3]);
+
+	return (0);
 }
