9fd05fda82a6645cbb74c272ebdf3d60927f10c7
bmoreira <bmoreira@student.42sp.org.br>
2026-08-14 22:53:10 -0300

    feat: started cpp03

---
+++ cpp03/ex00/include/ClapTrap.hpp
@@ -0,0 +1,15 @@
+#ifndef CLAPTRAP_HPP
+#define CLAPTRAP_HPP
+
+class ClapTrap {
+	private:
+		
+
+	public:
+		ClapTrap ();
+		ClapTrap ( const ClapTrap &other );
+		ClapTrap &operator= ( const ClapTrap &other );
+		~ClapTrap ();
+};
+
+#endif
---
+++ cpp03/ex00/src/ClapTrap.cpp
@@ -0,0 +1,17 @@
+#include "ClapTrap.hpp"
+
+ClapTrap::ClapTrap () {
+
+}
+
+ClapTrap::ClapTrap (const ClapTrap& other) {
+
+}
+
+ClapTrap& ClapTrap::operator= (const ClapTrap& other) {
+
+}
+
+ClapTrap::~ClapTrap () {
+
+}
