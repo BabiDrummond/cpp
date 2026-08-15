efaf51f1766d0f01892f90c4fc88442c5471206b
bmoreira <bmoreira@student.42sp.org.br>
2026-08-14 23:00:24 -0300

    feat/cpp03-ex00: implemented canonical form methods

--- cpp03/ex00/include/ClapTrap.hpp
+++ cpp03/ex00/include/ClapTrap.hpp
@@ -1,15 +1,23 @@
 #ifndef CLAPTRAP_HPP
 #define CLAPTRAP_HPP
 
+#include <string>
+
 class ClapTrap {
 	private:
-		
+		int	hitPoints;
+		int	energyPoints;
+		int	attackDamage;
 
 	public:
 		ClapTrap ();
 		ClapTrap ( const ClapTrap &other );
 		ClapTrap &operator= ( const ClapTrap &other );
 		~ClapTrap ();
+
+		void	attack(const std::string& target);
+		void	takeDamage(unsigned int amount);
+		void	beRepaired(unsigned int amount);
 };
 
 #endif
--- cpp03/ex00/src/ClapTrap.cpp
+++ cpp03/ex00/src/ClapTrap.cpp
@@ -1,17 +1,23 @@
 #include "ClapTrap.hpp"
 
-ClapTrap::ClapTrap () {
+ClapTrap::ClapTrap ():
+hitPoints(10),
+energyPoints (10),
+attackDamage (0) {}
 
-}
-
-ClapTrap::ClapTrap (const ClapTrap& other) {
-
-}
+ClapTrap::ClapTrap (const ClapTrap& other):
+hitPoints(10),
+energyPoints (10),
+attackDamage (0) {}
 
 ClapTrap& ClapTrap::operator= (const ClapTrap& other) {
-
+    if (this != &other) {
+        this->hitPoints = other.hitPoints;
+        this->energyPoints = other.energyPoints;
+        this->attackDamage = other.attackDamage;
+    }
+    return (*this);
 }
 
 ClapTrap::~ClapTrap () {
-
 }
