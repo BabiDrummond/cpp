b9bb4ef398b3375ec57bfedcbe204dbb4cc75410
bmoreira <bmoreira@student.42sp.org.br>
2026-09-18 22:21:40 -0300

    added more tests to cpp04/ex01

--- cpp04/ex01/src/main.cpp
+++ cpp04/ex01/src/main.cpp
@@ -6,6 +6,14 @@
 
 int main( void ) {
 	{
+		std::cout << "===============TESTS SUBJECT===============" << std::endl;
+		const Animal* j = new Dog();
+		const Animal* i = new Cat();
+		
+		delete j;//should not create a leak
+		delete i;
+	}
+	{
 		std::cout << "===============ANIMALS===============" << std::endl;
 		Animal *animals[10];
 
